011e3da27e184039b9f67fc6bd95282fe1099eef
Saswata Mishra <cs24mtech12001@iith.ac.in>
2024-12-18 20:27:17 +0530

    cse building pic added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import cse_building from "./assets/CSE-Building.jpg";
 
 
 function App() {
+
+  const Button = styled.button``
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section id='header-area'>
+        <header>
+          <figure>
+            <img src={cse_building} alt="CSE Building IITH" srcset="" id='cse_building'/>
+          </figure>
+        </header>
+      </section>
+
+      <section id="body">
+      
+      </section>
+
+      <section id="footer">
+
+      </section>
+    </>
   );
 }
 
