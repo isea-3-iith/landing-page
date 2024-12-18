9811a6fd4b0d34ffddf3cbdd28877452872d360b
Saswata Mishra <cs24mtech12001@iith.ac.in>
2024-12-18 22:28:04 +0530

    many new images added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import cse_building from "./assets/CSE-Building.jpg";
+import iith_logo from "./assets/CSELogo.png";
+import mnistry_logo from "./assets/meity-logo-2.png";
+import isea_logo from "./assets/ISEA.png";
 
 
 function App() {
@@ -14,8 +17,11 @@ function App() {
     <>
       <section id='header-area'>
         <header>
-          <figure>
-            <img src={cse_building} alt="CSE Building IITH" srcset="" id='cse_building'/>
+          <figure id="image-container">
+            <img src={isea_logo} alt="isea logo" srcset="" id='isea_logo'/>
+            <img src={cse_building} alt="CSE Building IITH" srcset="" id='cse_building' className='logo'/>
+            <img src={mnistry_logo} alt="ministry logo" srcset="" id='minisrty_logo'/>
+            <img src={iith_logo} alt="iith logo" srcset="" id='iith_logo'/>            
           </figure>
         </header>
       </section>
