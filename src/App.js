cfc848942ef55b906d27569697af8b1f93832f36
Saswata Mishra <cs24mtech12001@iith.ac.in>
2024-12-19 00:50:11 +0530

    further improvements

--- src/App.js
+++ src/App.js
@@ -35,17 +35,30 @@ function App() {
         <nav id='heading-area' className='montserrat-heading-zone'>
           <pre id='bootcamp-name' className='roboto-regular'>
             <p>A bootcamp on</p>
-            <h2>Wireless Security</h2>
+            <h1><em>Wireless Security</em></h1>
             <p>under ISEA Phase III</p>
           </pre>
           <div id='bootcamp-date'>
-            <p>27-31 January 2025</p>
+            <p id='date'>27 Jan - 1 Feb 2025</p>
           </div>
         </nav>
       </section>
 
+      <h2><i>WEBISITE TO BE UPDATED</i></h2>
       <section id="body">
-        <h2>WEBISITE TO BE UPDATED</h2>
+
+        <div id='main-area'>
+          <article id='about' className='about'>
+            <p className='about'>An oppertunity to learn from IITH faculty, deepen undertsnding of wireless network vulnurabilities and get participation certificates. Top performers in CTF/Projects have exciting rewards.</p>
+          </article>
+        </div>
+
+        <aside id='registration-details'>
+          <h2 id='registration-underlined'><u>Registration Details:</u></h2>
+          <ul>
+            <li>All BTech/MTech/PhD students from CS/AI/ECE/EE/IT backgrounds are welcome to register</li>
+          </ul>
+        </aside>
       </section>
 
       <section id="footer">
